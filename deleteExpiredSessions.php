04f4e0c69d1cbe7f08d7fde11934f5fd7c7ea457
Avinash <avinash.jairam@gmail.com>
2015-12-11 15:27:54 -0500

    delete expired sessions program complete. For now, it deletes folders (named after sessions) older than 3 minutes. Also, it deletes such records from the database

--- deleteExpiredSessions.php
+++ deleteExpiredSessions.php
@@ -1,58 +1,31 @@
 <?php
 
-	$link = mysqli_connect("localhost", "avi", "avi","cl55-steel");
+	//Database connection link
+	
 
+	//Array to hold the ids of the expired sessions
 	$sessionToDelete = [];
 
-	$currentTimeStampQuery = "SELECT * FROM `sessionInfo` WHERE `timeCreated` < (NOW() - INTERVAL 1 MINUTE)";
+	//SQl Query 
+	$currentTimeStampQuery = "SELECT * FROM `sessionInfo` WHERE `timeCreated` < (NOW() - INTERVAL 3 MINUTE)";
 	$result =  mysqli_query($link, $currentTimeStampQuery);
+
+	$sessionId;
 
 	//echo $result;
 	if(mysqli_num_rows($result) > 0){
 	 while($row = mysqli_fetch_array($result)){   
+	 	$sessionId = $row['sessionId'];
 	 	array_push($sessionToDelete, $row['sessionId']);
-	 	echo $row['sessionId'];
+	 	exec("rm -rf $sessionId");
 	 }
 	}
 	else{
 		echo "no rows found";
 	}
+
 	echo $sessionToDelete[0];
-	// $row    =  mysqli_fetch_array($result);
 
-	//   	 	while($row = mysqli_fetch_assoc($result)) {
-	//    	 		echo ($row['sessionId']);   	
- //   	 	}
-   	
-
-
-	//echo time();
-	//print_r($row);
-	//$currentTimeStamp = $row['current_timestamp'];
-	//echo $currentTimeStamp;
-
-	// $query  = "SELECT * FROM `sessionInfo`";
-	// $result = mysqli_query($link, $query);
-	// $row    = mysqli_fetch_array($result);
-
-	// $sessionId	 = array();
-	// $timeCreated = array();
-
-
-
-	// if (mysqli_num_rows($result) > 0) {
- //    // output data of each row
- //   	 	while($row = mysqli_fetch_assoc($result)) {
- //   	 		$sessionId.push($row['sessionId']);
- //   	 		$timeCreated.push($row['timeCreated']);
- //   	 	}
- //   	}
-
- //   	echo $sessionId[0] . " ".$timeCreated[0];
-	
-	
-
-
-
-
+	$deleteExpiredSessionsQuery = "DELETE FROM `sessionInfo` WHERE `timeCreated` < (NOW() - INTERVAL 1 MINUTE)";	
+	$result = mysqli_query($link, $deleteExpiredSessionsQuery);
 ?>
