f06e18b2b3bb8b36738a300bcb15e0556c326e01
Avinash <avinash.jairam@gmail.com>
2016-03-02 23:40:27 -0500

    Still adding comments to javascript code

--- userInterface.php
+++ userInterface.php
@@ -403,29 +403,15 @@ function getFileUploadError($error){
         <link href="./css/fileinput.css" media="all" rel="stylesheet" type="text/css" />
         <link href="./css/stylesheet1.css" media="all" rel="stylesheet" type="text/css" />
 
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-   
-
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script> 
         <script src="./js/plugins/canvas-to-blob.min.js" type="text/javascript"></script>
         <script src="./js/fileinput.min.js" type="text/javascript"></script>
-
-
-        <script src="./js/fileinput.js" type="text/javascript"></script>
-        <script src="./js/fileinput_locale_fr.js" type="text/javascript"></script>
-        <script src="./js/fileinput_locale_es.js" type="text/javascript"></script>
+        <script src="./js/fileinput.js" type="text/javascript"></script>     
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js" type="text/javascript"></script>
-    
-
-
-        
-     
-
-
     </head>
+
 <body>
-
-
-
+    <!-- navbar-->
     <div class="navbar navbar-default navbar-fixed-top ">
       <div class="container">
         <div class="navbar-header">
@@ -442,30 +428,16 @@ function getFileUploadError($error){
             <li><a href="./about.php">About</a></li>
             <li><a href="./contact.php">Contact</a></li>
           </ul>
-        </div><!--/.nav-collapse -->
+        </div>
       </div>
    </div>
  
 
     <div id="">
-      <!--  <div id="loading" class="row">
-        <div  class="col-sm-offset-5 col-sm-2 text-center">
-          <h3>Converting...</h3>
-          <img id="loading-image" src="./img/loading.gif" alt="Loading..." />
-        </div>
-       </div> -->
-
-     <!--   <div class="container contentContainer">
-            <div class= "row">
-                <div id = "download">
-                </div>
-            </div>
-       </div> -->
            
-        <div class="container contentContainer">            
-                <div class = "row">
-                                       
-                      <!--   <a href="" id ="downloadButton" class="btn btn-lg btn-success" download>Download My Converted MP4</a> -->
+        <div class="container contentContainer">  
+                <!-- Download link goes here -->          
+                <div class = "row">                                               
                       <a href="" id ="downloadButton" class="btn btn-lg btn-success" download><span class="glyphicon glyphicon-download-alt"></span> Click Here to Download Your Converted MP4</a>
                        <div class="col-md-6 col-md-offset-3" id="download">  
                           <br><br>
@@ -474,105 +446,97 @@ function getFileUploadError($error){
                           <source id="ogg_src" src="" type="video/ogg">
                           Your browser does not support HTML5 video.
                         </video>
-
-                      
-                        <!-- <a href="#" class="btn btn-lg btn-success"><span class="glyphicon glyphicon-arrow-down"></span> Download My Converted MP4 </a> -->
-                        <!-- <a href="" id ="downloadButton" class="btn btn-lg btn-success" download><span class="glyphicon glyphicon-arrow-down"></span> Download My Converted MP4 </a> -->
-
-                    </div>
+                     </div>
                 </div>
        
+            <!-- File upload section -->
              <div id="fileUpload" >         
-                <div class="row " >            
-                    <!-- <form method="post" action="userInterface.php">
-                       <label class="control-label">Select Audio File</label>
-                      <input id="input-7" name="fileUpload" multiple type="file" class="file file-loading text-center" data-allowed-file-extensions='["mp3", "wav", "m4a"]'> 
-                    </form> -->
-                    <div id="uploadTrack">
+                <div class="row " > 
+                   <!-- Track Upload -->        
+                   <div id="uploadTrack">
                         <br><br><br>
                         <form action="userInterface.php" method="post" enctype="multipart/form-data" >
                             <label >Select Track to upload:</label><br>
                             <input type="file" name="fileUpload" class="file" id="fileToUpload" data-allowed-file-extensions='["mp3"]'><br>
-                      <!--       <input type="submit" value="Upload Track" name="submit"> -->
+                      
                         </form>
                     </div>
                 </div>
-                <div class="row">
-                    <!-- <div class="col-md-2 col-md-offset-1" id="trackUploadSuccess"> -->
-                     <div class="col-sm-12" id="trackUploadSuccess">
-                       <!--  <img src="./img/successful-track-upload.JPG" alt="trackUploadSuccess"/> -->
-                        <!--  <a href="" id ="" class="btn btn-lg btn-success"><span class="glyphicon glyphicon-download-alt"></span> Track Uploaded Successfully</a> -->
+
+                <!--Successfull track upload message -->
+                <div class="row">                   
+                     <div class="col-sm-12" id="trackUploadSuccess">                       
                         <a href="#" class="btn btn-block btn-success"><span class="glyphicon glyphicon-ok"></span>  File Uploaded Successfully</a>
                          <br>
-
                     </div>
                 </div>
 
-                    
+                <!-- Ask user if he wants to upload image -->
+                <div id="imageOption">
 
-                    <div id="imageOption">
+                    <label >Would you like to add your own image to the mp4? If you don't, our default image will be used.</label>
+            
+                    <form action="userInterface.php" method="post">
+                      <input id="no" type="radio" name="image" value="no" checked onchange="showImageUpload(this)"> No
+                  
+                      <input id="yes" type="radio" name="image" value="yes" onchange="showImageUpload(this)"> Yes
+                    </form>
 
-                        <label >Would you like to add your own image to the mp4?</label>
-                
-                        <form action="userInterface.php" method="post">
-                          <input id="no" type="radio" name="image" value="no" checked onchange="showImageUpload(this)"> No
-                      
-                          <input id="yes" type="radio" name="image" value="yes" onchange="showImageUpload(this)"> Yes
-                        </form>
+                    <br><br>
 
-                        <br><br>
+                </div>
 
-                    </div>
-                <div class="row">
-                   <!--  <div class="col-md-2 col-md-offset-5" id="imageUploadSuccess"> -->
-                    <div class="" id="imageUploadSuccess">
-                        <!-- <img src="./img/successful-image-upload.JPG" alt="ImageUploadSuccess"/> -->
-                        <!-- <a href="" id ="" class="btn btn-lg btn-success" download><span class="glyphicon glyphicon-download-alt"></span> Image Uploaded Successfully</a> -->
+                <!--Successfull image upload message -->
+                <div class="row">              
+                    <div class="col-sm-12" id="imageUploadSuccess">                    
                         <a href="#" class="btn btn-block btn-success"><span class="glyphicon glyphicon-ok"></span> Image Uploaded Successfully </a>
                         <br>
                     </div>
                 </div>
 
-                   <div id="imageUpload" style="display:none">
-                       <form method="post" action="userInterface.php" enctype="multipart/form-data" >
-                           <label class="control-label ">Select Image</label>
-                           <input  type="file" name="image" class="file" data-allowed-file-extensions='["png", "gif", "jpg", "jpeg"]' >
-                        </form>
-                    </div>
-
-                     <br>
-                     
+                <!--Image Upload Form -->
+               <div id="imageUpload" style="display:none">
+                   <form method="post" action="userInterface.php" enctype="multipart/form-data" >
+                       <label class="control-label ">Select Image</label>
+                       <input  type="file" name="image" class="file" data-allowed-file-extensions='["png", "gif", "jpg", "jpeg"]' >
+                    </form>
                 </div>
+                    <br>
+                     
+            </div>
 
-                 <div id ="warning" class="alert alert-danger">
-                    <strong>Warning!</strong> Please Upload a Track First! 
-                 </div>
+            <!--Alert Messages -->
+             <div id ="warning" class="alert alert-danger">
+                <strong>Warning!</strong> Please Upload a Track First! 
+             </div>
 
-                  <div id ="warningImage" class="alert alert-danger">
-                    <strong>Warning!</strong> Please Upload an Image! If you don't wish to add an image, please check 'no.' Be aware that if you select this option a default image will be added to the video background.
-                 </div>
+              <div id ="warningImage" class="alert alert-danger">
+                <strong>Warning!</strong> Please Upload an Image! If you don't wish to add an image, please check 'no.' Be aware that if you select this option a default image will be added to the video background.
+             </div>
 
-                  <div id ="warningDuplicate" class="alert alert-danger">
-                    <strong>Warning!</strong> You uploaded that file already. Please upload a different file! 
-                 </div>
+              <div id ="warningDuplicate" class="alert alert-danger">
+                <strong>Warning!</strong> You uploaded that file already. Please upload a different file! 
+             </div>
 
-                 <div id ="warningLargeFile" class="alert alert-danger">
-                    <strong>Warning!</strong> The file you uploaded exceeds our limit of 1GB. Please upload a smaller file! 
-                 </div>
+             <div id ="warningLargeFile" class="alert alert-danger">
+                <strong>Warning!</strong> The file you uploaded exceeds our limit of 1GB. Please upload a smaller file! 
+             </div>
 
-                <br><br>
+            <br><br>
                 
-                    <div class="col-sm-offset-5 col-sm-2 text-center">
-                        <form method ="post" action="userInterface.php" onsubmit="return checkUpload()">
-                           <!--  <a href="#" class=""><span class=""></span> Convert!</a> -->
-                            <input type="submit" name = "convert" class="btn btn-block btn-lg btn-primary glyphicon glyphicon-wrench" id ="convert" value="Convert!" /> 
-                        </form>
-                    </div>
+                <!-- Convert Button. When the user presses this the checkUpload() JS function is called" -->
+                <div class="col-sm-offset-5 col-sm-2 text-center">
+                    <form method ="post" action="userInterface.php" onsubmit="return checkUpload()">
+                  
+                        <input type="submit" name = "convert" class="btn btn-block btn-lg btn-primary glyphicon glyphicon-wrench" id ="convert" value="Convert!" /> 
+                    </form>
+                </div>
                  
             </div>
 
         </div>
 
+            <!-- Footer -->
         <div>
             <footer class="footer">
               <div class="footerStyle">
@@ -589,8 +553,8 @@ function getFileUploadError($error){
 
     
  $(document).ready(function() {
-        //$('#loading').hide();
-
+       
+       //Hiding Various fields when the page loads
         document.getElementById('myVideo').style.display='none';
         document.getElementById('downloadButton').style.display='none';
         document.getElementById('trackUploadSuccess').style.display='none';
@@ -603,96 +567,52 @@ function getFileUploadError($error){
 
    
 
-        // $("#myVideo").hide();
-        // $("#downloadButton").hide();
-        // $("#trackUploadSuccess").hide();
-        // $("#imageUploadSuccess").hide();
-        // $("#warning").hide();
-        // $("#imageOption").hide();
-        // $("#warningImage").hide();
-        // $("#warningDuplicate").hide();
-        // $("#warningLargeFile").hide();
-        // $("#warningImage").hide();
-
-
         //TU means 'track uploaded'
         var TU = localStorage.getItem("trackUploaded");
 
-        //IU means 'image uploaded'
-     //   var IU = localStorage.getItem("imageUploaded");
-
-        //alert(TU);
-
-        // if(TU==1){
-        //     alert('Please upload a track first!');
-
-        // }
-
-        // if(trackUploaded==1 && imageUploaded == 1 && convertPressed == 0){
-        //     alert('Please upload a track first!');
-        // }
-
+  
+        //If a track is uploaded, ask the user if he wants to upload an image. Display the appropriate div 
          if(trackUploaded==0){
             hideUploadTrack();
             document.getElementById('imageOption').style.display='block';
         }
 
+        //If an image is uploaded, hide the upload track and image divs
         if(imageUploaded==0){
             hideUploadTrack();
             hideUploadImage();
         }
 
+        //If the user tries to upload a file which has already been uploaded, display the appropriate div
         if(duplicateValue==0){
             document.getElementById("warningDuplicate").style.display='block';
         }
 
+        //Show alert if an oversized file is uploaded
         if(overSizedTrack==0){
             document.getElementById("overSizedTrack").style.display='block';
         }
 
-        //if(larg)
-
+        //If the download flag is true, set and display the download link and div
         if(download==0){
             // hideMainContent();
-             var vid = document.getElementById("myVideo");
-             var downloadButton = document.getElementById("downloadButton");
-             var extension = ".mp4";
-            hideFileUploadContent();
-            var link=  downloadLink.concat(extension);
-            vid.src=link;
-            downloadButton.href=link;
-            //document.write(downloadLink);
-           // alert(downloadLink);
-            // //document.getElementById('download').innerHtml= downloadContent;
-            // document.querySelector("#myVideo > source").src = downloadLink;
+         var vid = document.getElementById("myVideo");
+         var downloadButton = document.getElementById("downloadButton");
+         var extension = ".mp4";
+         hideFileUploadContent();
+         var link=  downloadLink.concat(extension);
+         vid.src=link;
+         downloadButton.href=link;
 
-            // var video = document.getElementById('video');
-            // var source = document.createElement('source');
-
-            // source.setAttribute('src', downloadLink);
-
-            // video.appendChild(source);
-          //  video.load();
-
-
-
-            document.getElementById('myVideo').style.display='block';
-            document.getElementById('downloadButton').style.display='block';
-
-
+         document.getElementById('myVideo').style.display='block';
+         document.getElementById('downloadButton').style.display='block';
         }
-
-       
-
-
 
      });
 
     function checkUpload(){
-        var track=checkTrackUpload();
+       var track=checkTrackUpload();
        var image;
-        //alert(track);
-     //  alert("image = " + image + " track " + track); 
 
        if(track===true && IU == 'block'){
          image=checkImageUpload();
@@ -701,18 +621,10 @@ function getFileUploadError($error){
 
        if(track==true && IU == 'none'){
          return true;
-       }
-
-       //if (typeof image == 'undefined')
+       }    
 
         return false;
-
-        // if (track){
-        //     return true;
-        // }
-        // else{
-        //     return false; 
-        // }
+      
     }
 
 
@@ -830,22 +742,11 @@ function getFileUploadError($error){
 
 
 
-  
 
-
-    // $('#file-fr').fileinput({
-    //     language: 'fr',
-    //     uploadUrl: '#',
-    //     allowedFileExtensions : ['jpg', 'png','gif'],
-    // });
-    // $('#file-es').fileinput({
-    //     language: 'es',
-    //     uploadUrl: '#',
-    //     allowedFileExtensions : ['jpg', 'png','gif'],
-    // });
     $("#file-0").fileinput({
         'allowedFileExtensions' : ['jpg', 'png','gif'],
     });
+
     $("#file-1").fileinput({
         uploadUrl: '#', // you must set a valid URL here else you will get an error
         allowedFileExtensions : ['jpg', 'png','gif'],
@@ -857,11 +758,7 @@ function getFileUploadError($error){
             return filename.replace('(', '_').replace(']', '_');
         }
     });
-    /*
-    $(".file").on('fileselect', function(event, n, l) {
-        alert('File Selected. Name: ' + l + ', Num: ' + n);
-    });
-    */
+  
     $("#file-3").fileinput({
         showUpload: false,
         showCaption: false,
@@ -885,25 +782,14 @@ function getFileUploadError($error){
     $(".btn-info").on('click', function() {
         $('#file-4').fileinput('refresh', {previewClass:'bg-info'});
     });
-    /*
-    $('#file-4').on('fileselectnone', function() {
-        alert('Huh! You selected no files.');
-    });
-    $('#file-4').on('filebrowse', function() {
-        alert('File browse clicked for #file-4');
-    });
-    */
+  
     $(document).ready(function() {
         $("#test-upload").fileinput({
             'showPreview' : false,
             'allowedFileExtensions' : ['jpg', 'png','gif'],
             'elErrorContainer': '#errorBlock'
         });
-        /*
-        $("#test-upload").on('fileloaded', function(event, file, previewId, index) {
-            alert('i = ' + index + ', id = ' + previewId + ', file = ' + file.name);
-        });
-        */
+        
     });
   
 
